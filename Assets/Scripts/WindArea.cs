47a6e3ba21cbea327ecadad9ba6fef1c88d2097f
diammen <marcelnamdao@gmail.com>
2021-01-29 22:19:34 -0800

    Wind Zone scripts, add audio folder

---
+++ Assets/Scripts/WindArea.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindArea : MonoBehaviour
+{
+    [Header("GustTiming")]
+    public float startDelay;
+    public float cooldown;
+    public float gustScalar;
+    float timer;
+    int gustCounter;
+
+    [Header("Wind")]
+    public float strength;
+    public float strengthMultiplier;
+    public Vector3 direction;
+    float originalStrength;
+
+    public void Start()
+    {
+        timer = startDelay;
+        originalStrength = strength;
+    }
+    public void Update()
+    {
+        if (timer > 0)
+        {
+            timer -= Time.deltaTime;
+        }
+        else
+        {
+            strength = Random.Range(0f, 1f) * strengthMultiplier;
+            direction = Random.insideUnitSphere;
+            timer = cooldown;
+            gustCounter++;
+        }
+        strengthMultiplier = Mathf.Sqrt(gustCounter) * gustScalar;
+    }
+}
---
+++ Assets/Scripts/WindReceiver.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindReceiver : MonoBehaviour
+{
+    public WindArea windZone;
+    public bool inWindZone;
+
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (inWindZone)
+        {
+            rb.AddForce(windZone.direction * windZone.strength, ForceMode.Impulse);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("WindArea"))
+        {
+            windZone = other.GetComponent<WindArea>();
+            inWindZone = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("WindArea"))
+        {
+            inWindZone = false;
+        }
+    }
+}
