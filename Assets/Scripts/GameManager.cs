f56b77638bdbae4d4add6df70dca06295511a223
diammen <marcelnamdao@gmail.com>
2021-01-30 19:07:09 -0800

    Add main menu music, add flock prefab

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,16 +6,24 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     public string[] sceneNames;
+
+    public static GameManager instance;
+    Goal currentGoal;
     // Start is called before the first frame update
     void Start()
     {
         DontDestroyOnLoad(gameObject);
+        if (instance == null)
+            instance = this;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (currentGoal.goalHit)
+        {
+            Debug.Log("Hit");
+        }
     }
 
     public void LoadScene(string sceneName)
