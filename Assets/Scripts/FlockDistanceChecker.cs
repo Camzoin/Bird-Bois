52dd9eff6ad93f06292bb271a6c0cd2c21097f6e
Camzoin <Camden.Cecrle@gmail.com>
2021-01-31 11:03:04 -0800

    Worked on scene 3

---
+++ Assets/Scripts/FlockDistanceChecker.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlockDistanceChecker : MonoBehaviour
+{
+    public Transform bird, flock;
+
+    public float maxDistance;
+
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector3.Distance(bird.position, flock.position) > maxDistance)
+        {
+            //fail
+            animator.SetTrigger("Fail");
+        }
+    }
+}
