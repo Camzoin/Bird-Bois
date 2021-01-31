967922f956255c27fe79a7f19ebfa830d762ba3c
diammen <marcelnamdao@gmail.com>
2021-01-31 14:12:47 -0800

    Different stage 4

--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -8,7 +8,7 @@ public class Goal : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameManager.instance.currentGoal = this;
     }
 
     // Update is called once per frame
