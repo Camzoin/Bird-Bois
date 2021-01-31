92168f0ce13af51f81c3c883bd1ba5c6b606b793
Camzoin <Camden.Cecrle@gmail.com>
2021-01-30 20:50:34 -0800

    Added animations and vignette controller

---
+++ Assets/Scripts/VignetteController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VignetteController : MonoBehaviour
+{
+    public float timePerFrame = 1f;
+
+    float curFrameTime = 0f;
+
+    public SpriteRenderer vignette;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(curFrameTime < timePerFrame)
+        {
+            curFrameTime = curFrameTime + Time.deltaTime;
+        }
+        else
+        {
+            vignette.material.SetFloat("_Offset", Random.Range(0f, 100f));
+            curFrameTime = 0f;
+        }
+    }
+}
