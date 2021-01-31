7d7ffc218b26d482265a00478414ef5e3e19ccee
Camzoin <Camden.Cecrle@gmail.com>
2021-01-31 03:05:40 -0800

    Added transition animation from main menu to Stage 3

---
+++ Assets/Scripts/MenuBird.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuBird : MonoBehaviour
+{
+    public bool isGoing = false;
+    float time = 2, curTime = 0;
+    public Transform target;
+    public Vector3 startPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isGoing)
+        {
+            curTime = curTime + Time.deltaTime;
+            transform.position = Vector3.Lerp(startPos, target.position, curTime / time);
+            gameObject.GetComponent<Animator>().SetBool("isFlying", true);
+        }
+    }
+}
