b77c602e24869aeb63c0b6f492c6a02c9f4ec9c0
diammen <marcelnamdao@gmail.com>
2021-01-30 20:57:10 -0800

    Remove an extra mesh renderer component from bird prefab

--- Assets/Scripts/FollowTransform.cs
+++ Assets/Scripts/FollowTransform.cs
@@ -7,25 +7,17 @@ public class FollowTransform : MonoBehaviour
 {
     public Transform followTarget;
     public Vector3 offset;
-    public bool lockRotation;
 
-    Quaternion origRotation;
     // Start is called before the first frame update
     void Start()
     {
-        if (lockRotation)
-        {
-            origRotation = transform.rotation;
-        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (lockRotation)
-        {
-            transform.rotation = origRotation;
-        }
+
         transform.position = followTarget.position + offset;
     }
 }
